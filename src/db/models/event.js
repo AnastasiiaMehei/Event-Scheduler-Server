9229ff5db1da7ad164f7b83295158c057d34da51
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2025-01-18 15:23:18 +0200

    model

--- src/db/models/event.js
+++ src/db/models/event.js
@@ -2,7 +2,8 @@ import { model, Schema } from 'mongoose';
 
 const eventSchema = new Schema(
   {
-    eventId: { type: String, required: false, unique: true },
+    _id: { type: Schema.Types.ObjectId, auto: true },
+    eventId: { type: String, required: false, unique: true }, // Use eventId for UUID
     name: { type: String, required: true, maxlength: 100 },
     date: { type: Date, required: true },
     time: { type: String, required: true, match: /^\d{2}:\d{2}$/ },
